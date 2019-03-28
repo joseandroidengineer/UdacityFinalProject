433fcb98295449412ceb18a87981ed7a586d4c20
Jose Escalante <jesca15341@gmail.com>
2019-03-27 21:10:36 -0400

    Removed anything that had to do with ads from the paid flavor of the application. Fixed up the test to test our asynctask

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTest.java
@@ -4,22 +4,17 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.annotation.UiThreadTest;
 import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
 import android.util.Pair;
 
 import junit.framework.TestCase;
 
-import org.junit.Before;
 import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
-import static junit.framework.TestCase.assertNotNull;
-import static junit.framework.TestCase.assertTrue;
 
 public class AsyncTest extends TestCase implements JokeTaskListener{
 
@@ -33,7 +28,7 @@ public class AsyncTest extends TestCase implements JokeTaskListener{
     protected void setUp() throws Exception {
         super.setUp();
         activityTestRule = new ActivityTestRule<>(MainActivity.class);
-        context = InstrumentationRegistry.getContext();
+        context = InstrumentationRegistry.getTargetContext();
         signal = new CountDownLatch(1);
         endpointsAsyncTask = new EndpointsAsyncTask(this);
     }
--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -3,9 +3,7 @@ package com.udacity.gradle.builditbigger;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.util.Log;
 import android.util.Pair;
-import android.view.View;
 import android.widget.Toast;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
@@ -57,14 +55,13 @@ public class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, S
             try {
                 return myApiService.sayHi(name).execute().getData();
             } catch (IOException e) {
-                return e.getMessage();
+                return null;
             }
         }
 
         @Override
         protected void onPostExecute(String result) {
             Toast.makeText(context, result, Toast.LENGTH_LONG).show();
-            Log.e("RESULT",result);
             Intent intent = new Intent(context, JokeActivity.class);
             intent.putExtra("joke",result);
             jokeTaskListener.jokeTaskCompleted();
